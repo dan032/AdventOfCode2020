88aa9891424f6e190390e4c39721fa026f11f7fa
Daniel Butts <buttsda@sheridancollege.ca>
2020-12-03 01:33:19 -0500

    Day 3 Solutions

---
+++ day3/day3a.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+    string line;
+    string filename = R"(C:\Users\Dan\Desktop\AOC2020\day3\input.txt)";
+    ifstream file(filename);
+    vector<string> v;
+
+    if (!file.is_open())
+    {
+        perror("File not found");
+        return EXIT_FAILURE;
+    }
+
+    while(file.good())
+    {
+        getline(file, line);
+        v.push_back(line);
+    }
+
+    int xIdx = 0, yIdx = 0, count = 0;
+    int width = v[0].size() - 1;
+
+    while(yIdx < v.size())
+    {
+        if (v[yIdx][xIdx] == '#')
+            count++;
+
+        yIdx += 1;
+        xIdx += 3;
+        if (xIdx >= width) xIdx -= width;
+    }
+
+    cout << count << endl;
+    file.close();
+    return EXIT_SUCCESS;
+}
---
+++ day3/day3b.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+    string line;
+    string filename = R"(C:\Users\Dan\Desktop\AOC2020\day3\input.txt)";
+    ifstream file(filename);
+    vector<string> v;
+
+    if (!file.is_open())
+    {
+        perror("File not found");
+        return EXIT_FAILURE;
+    }
+
+    while(file.good())
+    {
+        getline(file, line);
+        v.push_back(line);
+    }
+
+    int values[5][2] = {{1,1},{3,1},{5,1},{7,1},{1,2}};
+
+    long total = 1;
+    int width = v[0].size() - 1;
+    int xIdx = 0, yIdx = 0, count = 0;
+
+    for (auto & value : values)
+    {
+        xIdx = 0;
+        yIdx = 0;
+        count = 0;
+
+        while(yIdx < v.size())
+        {
+            if (v[yIdx][xIdx] == '#')
+            {
+                count++;
+                cout << yIdx << ", " << xIdx << endl;
+            }
+
+            yIdx += value[1];
+            xIdx += value[0];
+            if (xIdx >= width) xIdx -= width;
+        }
+        cout << count << endl << endl;
+        total *= count;
+    }
+
+    cout << total << endl;
+    file.close();
+    return EXIT_SUCCESS;
+}
