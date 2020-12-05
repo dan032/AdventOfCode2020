c56edba108f0a57f7e82f48a74b3e832d2b6cb52
Daniel Butts <buttsda@sheridancollege.ca>
2020-12-05 01:05:40 -0500

    Day 5 Solutions

---
+++ day5/day5a.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+    string line;
+    string filename = R"(C:\Users\Dan\Desktop\AOC2020\day5\input.txt)";
+    ifstream file(filename);
+
+    if (!file.is_open())
+    {
+        perror("File not found");
+        return EXIT_FAILURE;
+    }
+
+    int res = 0;
+    while(file.good()) {
+        getline(file, line);
+
+        int l = 0, r = 127;
+        for (int i = 0; i < 6; i++)
+        {
+            if (line[i] == 'F')
+            {
+                r -= (r-l + 1)/2;
+            }
+            else if (line[i] == 'B') {
+                l += (r - l + 1) / 2;
+            }
+        }
+
+        int row = line[6]  == 'F' ? min(l,r) : max(l,r);
+
+        l = 0;
+        r = 7;
+        for (int i = 6; i < 9; i++)
+        {
+            if (line[i] == 'R')
+            {
+                l += (r-l + 1)/2;
+            }
+            else if (line[i] == 'L') {
+                r -= (r - l + 1) / 2;
+            }
+        }
+
+        int col = line[9] == 'R' ? max(l,r) : min(l,r);
+        res = max(res, (row * 8 ) + col);
+    }
+
+    cout << res << endl;
+
+    file.close();
+    return EXIT_SUCCESS;
+}
---
+++ day5/day5b.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string line;
+    string filename = R"(C:\Users\Dan\Desktop\AOC2020\day5\input.txt)";
+    ifstream file(filename);
+    vector<int> v;
+
+    if (!file.is_open())
+    {
+        perror("File not found");
+        return EXIT_FAILURE;
+    }
+
+    while(file.good()) {
+        getline(file, line);
+
+        int l = 0, r = 127;
+        for (int i = 0; i < 6; i++)
+        {
+            if (line[i] == 'F')
+            {
+                r -= (r-l + 1)/2;
+            }
+            else if (line[i] == 'B') {
+                l += (r - l + 1) / 2;
+            }
+        }
+
+        int row = line[6]  == 'F' ? min(l,r) : max(l,r);
+
+        l = 0;
+        r = 7;
+        for (int i = 6; i < 9; i++)
+        {
+            if (line[i] == 'R')
+            {
+                l += (r-l + 1)/2;
+            }
+            else if (line[i] == 'L') {
+                r -= (r - l + 1) / 2;
+            }
+        }
+
+        int col = line[9] == 'R' ? max(l,r) : min(l,r);
+
+        v.push_back((row * 8 ) + col);
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < v.size() - 1; i++)
+    {
+        if (v[i + 1] - v[i] > 1)
+        {
+            cout << (v[i + 1] + v[i])/2 << endl;
+        }
+    }
+
+
+    file.close();
+    return EXIT_SUCCESS;
+}
