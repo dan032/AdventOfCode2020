baddc7bc0704949cb813edeeac012d832ca14309
Daniel Butts <buttsda@sheridancollege.ca>
2020-12-06 18:52:51 -0500

    Day 6 Solutions

--- day6/day6a.cpp
+++ day6/day6a.cpp
@@ -9,6 +9,7 @@ int main()
     string line;
     string filename = R"(C:\Users\Dan\Desktop\AOC2020\day6\input.txt)";
     ifstream file(filename);
+    int total = 0;
 
     if (!file.is_open())
     {
@@ -16,28 +17,27 @@ int main()
         return EXIT_FAILURE;
     }
 
-    int total = 0;
+    unordered_set<char> m;
     while(file.good())
     {
-        bool firstEnter = true;
-        unordered_set<char> m;
-
-        while (firstEnter || ((line != "\r") && file.good()))
+        getline(file, line);
+        if (line == "\r")
         {
-            getline(file, line);
-            for (char c : line)
+            total += m.size();
+            m.clear();
+        }
+
+        for (char c : line)
+        {
+            if (c == '\r') continue;
+            if (m.find(c) == m.end())
             {
-                if (c == '\r') continue;
-                if (m.find(c) == m.end())
-                {
-                    m.insert(c);
-                    total++;
-                }
+                m.insert(c);
             }
-            firstEnter = false;
         }
     }
 
+    total += m.size();
     cout << total << endl;
     file.close();
     return EXIT_SUCCESS;
