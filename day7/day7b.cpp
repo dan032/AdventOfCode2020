65999bc023e90084cfb33b1a8538d26d526dc73a
Daniel Butts <buttsda@sheridancollege.ca>
2020-12-07 17:00:34 -0500

    Day 7 Solutions

--- day7/day7b.cpp
+++ day7/day7b.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <unordered_set>
 #include <unordered_map>
 
 using namespace std;
-void checkMap(unordered_map<string, unordered_map<string, int>>, const unordered_map<string, int>&);
+
+typedef struct {
+    unordered_map<string, int> subBags;
+} Bag;
+
+
+void checkBag(unordered_map<string,Bag> m, const unordered_map<string, int> &current);
+
 int total = 0;
 
 int main()
 {
     string line;
     string filename = R"(C:\Users\Dan\Desktop\AOC2020\day7\input.txt)";
-    unordered_map<string, unordered_map<string, int>> m;
-
     ifstream file(filename);
+
+    unordered_map<string, Bag> m;
 
     if (!file.is_open())
     {
@@ -30,8 +35,7 @@ int main()
         string key = line.substr(0, line.find("bag") - 1);
         string remainder = line.substr(line.find(key) + key.length() + 5);
 
-        // Each entry in the map will contain the key, and another map that will hold the bags it holds, and their amounts
-        m[key] = unordered_map<string,int>();
+        m[key] = Bag();
 
         for (int i = 0; i < remainder.length(); i++)
         {
@@ -40,28 +44,28 @@ int main()
                 int value = (int) remainder[i] - '0';
                 int endIndex = remainder.find("bag", i);
                 string bag = remainder.substr(i + 2, endIndex - i - 3);
-                m[key][bag] = value;
+                m[key].subBags[bag] = value;
             }
         }
     }
 
     // Recursively go through the map, starting with shiny gold.
-    unordered_map<string, int> sg = m["shiny gold"];
-    checkMap(m, sg);
+    unordered_map<string, int> sg = m["shiny gold"].subBags;
+    checkBag(m, sg);
 
     file.close();
     cout << "Total " << total << endl;
     return EXIT_SUCCESS;
 }
 
-void checkMap(unordered_map<string, unordered_map<string, int>> m, const unordered_map<string, int>& current)
+void checkBag(unordered_map<string, Bag> m, const unordered_map<string, int>& current)
 {
     for (auto& e : current)
     {
         for (int i = 0; i < e.second; i++)
         {
             total++;
-            checkMap(m, m[e.first]);
+            checkBag(m, m[e.first].subBags);
         }
     }
 }
