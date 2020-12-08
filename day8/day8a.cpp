f5ae7a6206518cf5e1a3100df8d03961da0e9e95
Daniel Butts <buttsda@sheridancollege.ca>
2020-12-08 09:32:33 -0500

    Day 8 Solutions

--- day8/day8a.cpp
+++ day8/day8a.cpp
@@ -4,16 +4,17 @@
 #include <unordered_set>
 
 using namespace std;
-void findAccumulatorValue(vector<pair<string, int>>);
-
-int acc = 0;
+void findAccumulatorValue(vector<pair<string, int>>, int&);
 
 int main()
 {
-    string line;
     string filename = R"(C:\Users\Dan\Desktop\AOC2020\day8\input.txt)";
-    vector<pair<string, int>> v;
     ifstream file(filename);
+
+    string line, operation;
+    int value = 0, acc = 0;
+    vector<pair<string, int>> v;
+    pair<string, int> opPair;
 
     if (!file.is_open())
     {
@@ -21,54 +22,37 @@ int main()
         return EXIT_FAILURE;
     }
 
-    while(file.good())
+    while(file.good())                  // Populate vector with all op codes and their values
     {
         getline(file, line);
-        string op = line.substr(0, line.find(' '));
-        int num = stoi(line.substr((line.find(' '))));
-        pair<string, int> p =  make_pair(op, num);
-        v.push_back(p);
+        operation = line.substr(0, line.find(' '));
+        value = stoi(line.substr((line.find(' '))));
+        opPair = make_pair(operation, value);
+        v.push_back(opPair);
     }
 
-    findAccumulatorValue(v);
+    findAccumulatorValue(v, acc);
 
     cout << acc << endl;
     file.close();
     return EXIT_SUCCESS;
 }
 
-void findAccumulatorValue(vector<pair<string, int>> v)
+void findAccumulatorValue(vector<pair<string, int>> v, int &acc)
 {
-    unordered_set<int> m;
+    unordered_set<int> m;      // Will store index to check if op code has already been called
     for (int i = 0; i < v.size(); i++)
     {
+        if (m.find(i) != m.end()) return;
+
+        m.insert(i);
         if (v[i].first == "acc")
         {
-            if (m.find(i) != m.end())
-            {
-                break;
-            }
-            m.insert(i);
             acc += v[i].second;
         }
         else if (v[i].first == "jmp")
         {
-            if (m.find(i) != m.end())
-            {
-                break;
-            }
-            m.insert(i);
-
             i += v[i].second - 1;
-        }
-
-        else if (v[i].first == "nop")
-        {
-            if (m.find(i) != m.end())
-            {
-                break;
-            }
-            m.insert(i);
         }
     }
 }
