904bf001dd6713a60f4a7f3d0c7ed09bcb06d2fa
Daniel Butts <buttsda@sheridancollege.ca>
2020-12-08 09:22:08 -0500

    Day 8 Solutions

--- day8/day8b.cpp
+++ day8/day8b.cpp
@@ -4,19 +4,19 @@
 #include <unordered_set>
 
 using namespace std;
+bool checkIntcode(vector<pair<string, int>> v, bool initializeOpsVectors, int&);
 
-bool checkIntcode(vector<pair<string, int>> v, bool initializeVectors);
-
-int acc = 0;                        // Accumulator for final result
-vector<int> jmps;                   // Will be used to brute force all jump op codes
-vector<int> nops;                   // Will be used to brute force all nop op codes
+vector<int> opsVector;
 
 int main()
 {
-    string line;
     string filename = R"(C:\Users\Dan\Desktop\AOC2020\day8\input.txt)";
-    vector<pair<string, int>> v;
     ifstream file(filename);
+
+    string line, operation;
+    int acc = 0, value = 0;
+    vector<pair<string, int>> v;
+    pair<string, int> opPair;
 
     if (!file.is_open())
     {
@@ -24,90 +24,66 @@ int main()
         return EXIT_FAILURE;
     }
 
-    while(file.good())          // Populate vector with all op codes and their values
+    while(file.good())                  // Populate vector with all op codes and their values
     {
         getline(file, line);
-        string op = line.substr(0, line.find(' '));
-        int num = stoi(line.substr((line.find(' '))));
-        pair<string, int> p =  make_pair(op, num);
-        v.push_back(p);
+        operation = line.substr(0, line.find(' '));
+        value = stoi(line.substr((line.find(' '))));
+        opPair = make_pair(operation, value);
+        v.push_back(opPair);
     }
-
-    checkIntcode(v, true);     // Initializes vectors that will be used for brute forcing
-
-    bool pass = false;
-    for (int jmp : jmps)         // Brute force to see if changing jump ops removes infinite loop
-    {
-        v[jmp].first = "nop";
-        acc = 0;
-        bool infinite = checkIntcode(v, false);
-        if (!infinite){
-            pass = true;
-            break;
-        }
-        v[jmp].first ="jmp";
-    }
-
-    if (!pass)
-    {
-        for (int nop : nops)     // Brute force to see if changing nop ops removes infinite loop
-        {
-            acc = 0;
-            v[nop].first = "jmp";
-            bool infinite = checkIntcode(v, false);
-            if (!infinite){
-                break;
-            }
-            v[nop].first ="nop";
-        }
-    }
-
-    cout << acc << endl;
     file.close();
+    checkIntcode(v, true, acc);     // Initializes vector that will be used for brute forcing
+
+    bool infinite = true;
+    for (int op : opsVector)         // Brute force to see if changing ops removes infinite loop
+    {
+        acc = 0;
+        if (v[op].first == "jmp")
+        {
+            v[op].first = "nop";
+            infinite = checkIntcode(v, false, acc);
+            v[op].first ="jmp";
+        }
+        else if (v[op].first == "nop")
+        {
+            v[op].first = "jmp";
+            infinite = checkIntcode(v, false, acc);
+            v[op].first ="nop";
+        }
+
+        if (!infinite) break;
+    }
+    cout << acc << endl;
     return EXIT_SUCCESS;
 }
 
-bool checkIntcode(vector<pair<string, int>> v, bool initializeVectors)
+bool checkIntcode(vector<pair<string, int>> v, bool initializeOpsVectors, int &acc)
 {
-    unordered_set<int> m;      // Will be stored to check if op code has already been called
-
+    unordered_set<int> m;      // Will store index to check if op code has already been called
     for (int i = 0; i < v.size(); i++)
     {
+        if (m.find(i) != m.end()) return true;
+
+        m.insert(i);
         if (v[i].first == "acc")
         {
-            if (m.find(i) != m.end())
-            {
-                return true;
-            }
-            m.insert(i);
             acc += v[i].second;
         }
         else if (v[i].first == "jmp")
         {
-            if (m.find(i) != m.end())
+            if (initializeOpsVectors)
             {
-                return true;
+                opsVector.push_back(i);
             }
-            if (initializeVectors)
-            {
-                jmps.push_back(i);
-            }
-
-            m.insert(i);
             i += v[i].second - 1;
         }
-
         else if (v[i].first == "nop")
         {
-            if (m.find(i) != m.end())
+            if (initializeOpsVectors)
             {
-                return true;
+                opsVector.push_back(i);
             }
-            if (initializeVectors)
-            {
-                nops.push_back(i);
-            }
-            m.insert(i);
         }
     }
     return false;
