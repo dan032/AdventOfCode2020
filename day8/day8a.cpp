0ddd82f76d17a0fc6348c204e6b27b26ac61a82c
Daniel Butts <buttsda@sheridancollege.ca>
2020-12-08 20:25:37 -0500

    Refactored code into IntCode Machine in case it needs to be built upon later.

--- day8/day8a.cpp
+++ day8/day8a.cpp
@@ -2,16 +2,14 @@
 #include <cassert>
 #include "../IntMachine/IntMachine.h"
 
-using namespace std;
-
 int main()
 {
-    string filename = R"(C:\Users\Dan\Desktop\AOC2020\day8\input.txt)";
+    std::string filename = R"(C:\Users\Dan\Desktop\AOC2020\day8\input.txt)";
     IntMachine machine = IntMachine(filename);
-    int successful = machine.parseInput();
+    int successful = machine.ParseInput();
     assert(successful != -1);
 
-    machine.analyzeInfiniteLoop(false);
-    cout << machine.getAccumulator() << endl;
+    machine.AnalyzeInfiniteLoop(false);
+    std::cout << machine.GetAccumulator() << std::endl;
     return EXIT_SUCCESS;
 }
--- day8/day8b.cpp
+++ day8/day8b.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 #include <cassert>
 #include "../IntMachine/IntMachine.h"
-using namespace std;
-
 
 int main()
 {
-    string filename = R"(C:\Users\Dan\Desktop\AOC2020\day8\input.txt)";
+    std::string filename = R"(C:\Users\Dan\Desktop\AOC2020\day8\input.txt)";
     IntMachine machine = IntMachine(filename);
-    int successful = machine.parseInput();
+    int successful = machine.ParseInput();
     assert(successful != -1);
 
-    machine.analyzeInfiniteLoop(true);
-    machine.tryRemoveInfiniteLoop();
-    cout << machine.getAccumulator() << endl;
+    machine.AnalyzeInfiniteLoop(true);
+    machine.TryRemoveInfiniteLoop();
+    std::cout << machine.GetAccumulator() << std::endl;
     return EXIT_SUCCESS;
 }
 
