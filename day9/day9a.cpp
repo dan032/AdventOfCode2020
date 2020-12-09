91af1d93082f342f688b6df0cb8ff588d342bbc4
Daniel Butts <buttsda@sheridancollege.ca>
2020-12-09 16:07:41 -0500

    Day 9 Solutions - Removed deque from A.

--- day9/day9a.cpp
+++ day9/day9a.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <unordered_set>
-#include <deque>
 
 using namespace std;
-bool checkSum(deque<long>  q, vector<long> v, int r);
+bool checkSum(vector<long> v, int l, int r);
 
 int main()
 {
     string line;
     string filename = R"(C:\Users\Dan\Desktop\AOC2020\day9\input.txt)";
     vector<long> v;
-    deque<long> q;
 
     ifstream file(filename);
 
@@ -28,18 +25,14 @@ int main()
         v.push_back(stol(line));
     }
 
+    int l = 0;
     int r = 25;
-
-    for (int i = 0; i < r; i++)
-    {
-        q.push_back(v[i]);
-    }
 
     bool success;
     while(r < v.size())
     {
 
-        success = checkSum(q, v, r);
+        success = checkSum(v, l, r);
 
         if (!success)
         {
@@ -47,22 +40,22 @@ int main()
             break;
         }
 
-        q.push_back(v[r++]);
-        q.pop_front();
+        l++;
+        r++;
     }
 
     file.close();
     return EXIT_SUCCESS;
 }
 
-bool checkSum(deque<long>  q, vector<long> v, int r)
+bool checkSum(vector<long> v, int l, int r)
 {
     bool found = false;
-    for (int i = 0; i < q.size(); i++)
+    for (int i = l; i < r; i++)
     {
-        for (int j = i + 1; j < q.size(); j++)
+        for (int j = i + 1; j < r; j++)
         {
-            if (q[i] + q[j] == v[r])
+            if (v[i] + v[j] == v[r])
             {
                 found = true;
             }
